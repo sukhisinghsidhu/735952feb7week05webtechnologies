1fe7e2128b22433704aace4b2cbf0ff6a4abfef6
visualstudio <visualstudio@230-PC03>
2019-02-07 11:43:06 -0500

    1145 am commit

--- 735952feb7week05webtechnologies/js/main.js
+++ 735952feb7week05webtechnologies/js/main.js
@@ -4,3 +4,9 @@ function myFunction(a, b) {
     //var b = 6;
     return a + b;
 }
+
+function arrays() {
+    var pens;
+    pens = ["red", "orage", "purple", "lilac", "satrangi"];
+    console.log(pens);
+}
